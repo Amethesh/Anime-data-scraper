3cdce47d51c13b8c0bf6254d04ca015848ff528a
Amethesh LGP <ametheshlgp@gmail.com>
2023-06-14 23:29:16 +0530

    Testing zoro, crunchyroll and others servers link

--- episodesLinks.js
+++ episodesLinks.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Using the example query "demon", and looking at the 2nd page of results.
-const url = "https://api.consumet.org/anime/gogoanime/demon?page=1";
+const url = "https://api.consumet.org/anime/gogoanime/watch/one-piece-episode-1";
 const data = async () => {
  try {
- const { data } = await axios.get(url, { params: { page: 2 } });
+ const { data } = await axios.get(url, { params: { server: "streamsb" } });
  return data;
  } catch (err) {
  throw new Error(err.message);

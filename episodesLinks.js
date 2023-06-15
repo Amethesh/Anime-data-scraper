ac6a8ce4f8b5799c1dac8a4f2be7103c916bec39
Amethesh LGP <ametheshlgp@gmail.com>
2023-06-15 22:02:02 +0530

    Adding vidstreaming links in Json file format

--- episodesLinks.js
+++ episodesLinks.js
@@ -1,17 +1,46 @@
 import axios from "axios";
+import fs from "fs";
 
-// Using the example query "demon", and looking at the 2nd page of results.
-const url = "https://api.consumet.org/anime/gogoanime/watch/one-piece-episode-1";
-const data = async () => {
- try {
- const { data } = await axios.get(url, { params: { server: "streamsb" } });
- return data;
- } catch (err) {
- throw new Error(err.message);
- }
+const fetchData = async (episodeNo) => {
+  const url = `https://api.consumet.org/anime/gogoanime/watch/one-piece-episode-${episodeNo}`;
+  try {
+    const { data } = await axios.get(url, {
+      params: { server: "vidstreaming" },
+    });
+    return data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
-const info = await data()
+const writeEpisodeDataToFile = async () => {
+  const episodes = 20;
+  const jsonFile = "episode_data.json";
 
+  try {
+    let jsonData = [];
 
-console.log(info);
+    // Check if the JSON file already exists
+    if (fs.existsSync(jsonFile)) {
+      const existingData = fs.readFileSync(jsonFile);
+      jsonData = JSON.parse(existingData);
+    }
+
+    for (let episodeNo = 11; episodeNo <= episodes; episodeNo++) {
+      const data = await fetchData(episodeNo);
+      const episodeData = {
+        episodeNumber: episodeNo,
+        vidstreaming: data,
+      };
+      jsonData.push(episodeData);
+      console.log(`Episode ${episodeNo} processed.`);
+    }
+
+    fs.writeFileSync(jsonFile, JSON.stringify(jsonData, null, 2));
+    console.log(`Data written to ${jsonFile} successfully.`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+writeEpisodeDataToFile();
